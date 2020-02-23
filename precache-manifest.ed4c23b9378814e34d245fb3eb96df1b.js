1d3c0b8569051fcafcae8a2578febe2db235130c
Tuan Nguyen <tuan.nguyenle@nashtechglobal.com>
2020-02-23 13:11:49 +0700

    Updates

---
+++ precache-manifest.ed4c23b9378814e34d245fb3eb96df1b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e96a486204ff57e2541f",
+    "url": "/minesweeper/static/js/runtime~main.e96a4862.js"
+  },
+  {
+    "revision": "8e0873f52bf19cf3beb4",
+    "url": "/minesweeper/static/js/main.8e0873f5.chunk.js"
+  },
+  {
+    "revision": "d11cbbd9c3df872be0ca",
+    "url": "/minesweeper/static/js/1.d11cbbd9.chunk.js"
+  },
+  {
+    "revision": "8e0873f52bf19cf3beb4",
+    "url": "/minesweeper/static/css/main.d4a998a5.chunk.css"
+  },
+  {
+    "revision": "72796ff284cbced17813292364af4a80",
+    "url": "/minesweeper/index.html"
+  }
+];
